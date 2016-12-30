5839fe6a80e049f56ea2d2995b6cdcd895b841e7
Aaron <aaron@toumetis.com>
2016-12-30 11:04:34 +0000

    tidied up some PEP 8 formatting issues

--- lamda_function.py
+++ lamda_function.py
@@ -7,24 +7,39 @@ import requests
 # --------------- Helpers that build all of the responses ----------------------
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'card': {
-            'type': 'Simple',
-            'title': title,
-            'content': output
-        },
-        'reprompt': {
+    if title is not None:
+        return {
             'outputSpeech': {
                 'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
-    }
+                'text': output
+                },
+            'reprompt': {
+                'outputSpeech': {
+                    'type': 'PlainText',
+                    'text': reprompt_text
+                }
+            },
+            'shouldEndSession': should_end_session
+        }
+    else:
+        return {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': output
+            },
+            'card': {
+                'type': 'Simple',
+                'title': title,
+                'content': output
+            },
+            'reprompt': {
+                'outputSpeech': {
+                    'type': 'PlainText',
+                    'text': reprompt_text
+                }
+            },
+            'shouldEndSession': should_end_session
+        }
 
 
 def build_response(session_attributes, speechlet_response):
@@ -35,7 +50,7 @@ def build_response(session_attributes, speechlet_response):
     }
 
 
-# --------------- Functions that control the skill's behavior ------------------
+# --------------- Functions that control the skill's behavior -----------------
 
 
 def get_welcome_response():
@@ -47,28 +62,18 @@ def get_welcome_response():
     res = requests.get('https://idunno.io/api/get_questions')
     questions = res.json()['questions']
     session_attributes['questions'] = list(questions)
-    session_attributes['tags'] = {}
     session_attributes['yes_list'] = []
     session_attributes['no_list'] = []
     question_object = session_attributes['questions'][session_attributes['stage']]
     question = question_object['question_text']
-    card_title = "Welcome to iDunno. "
-    speech_output = "Welcome to iDunno. Would you like to {}? ".format(question)
+    card_title = None
+    speech_output = "Let's find something for you to do. Would you like to {}? ".format(question)
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Sooooo. "
+    reprompt_text = speech_output
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
-
-
-def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = "See ya! "
-    # Setting this to true ends the session and exits the skill.
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
 
 
 def answer_question(intent, session):
@@ -78,15 +83,13 @@ def answer_question(intent, session):
     stage = session['attributes']['stage']
     if 'response' in intent['slots']:
         answer = intent['slots']['response']['value']
-        tag = session['attributes']['questions'][stage]['tag_name']
-        session['attributes']['tags'][tag] = answer
-        if 'response'== 'yes':
-            session['attributes']['yes_list'] = session['attributes']['questions'][stage]['id']
-        elif 'response' == 'no':
-            session['attributes']['no_list'] = session['attributes']['questions'][stage]['id']
+        if answer == 'yes':
+            session['attributes']['yes_list'].append(session['attributes']['questions'][stage]['id'])
+        elif answer == 'no':
+            session['attributes']['no_list'].append(session['attributes']['questions'][stage]['id'])
     session['attributes']['stage'] += 1
     new_stage = session['attributes']['stage']
-    card_title = intent['name']
+    card_title = None
     should_end_session = False
     if new_stage < 5:
         question_object = session_attributes['questions'][new_stage]
@@ -94,23 +97,43 @@ def answer_question(intent, session):
         speech_output = "Would you like to {}? ".format(question)
         reprompt_text = speech_output
     else:
-        payload = {'yes_list': session['attributes']['yes_list'], 'no_list': session['attributes']['no_list']}
-        res = requests.get('https://idunno.io/api/get_activities', params=payload)
-        decision = res.json()['activity_description']
-        speech_output = "iDunno thinks you should {}? ".format(decision)
+        yes_list = str(session['attributes']['yes_list'])
+        no_list = str(session['attributes']['no_list'])
+        payload = {'yes_list': yes_list, 'no_list': no_list}
+        res = requests.get('https://idunno.io/api/alexa_get_activities', params=payload)
+        decision = res.json()['activity_desc']
+        speech_output = "iDunno thinks you should {}. ".format(decision)
         reprompt_text = speech_output
         should_end_session = True
+        card_title = "iDunno says..."
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def get_help(intent, session):
+    card_title = "How to use iDunno"
+    session_attributes = session['attributes']
+    should_end_session = False
+
+    speech_output = "Just open the skill and answer the five questions to hear a suggestion from iDunno. "
+    reprompt_text = speech_output
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def exit_skill(intent, session):
+    card_title = None
+    session_attributes = session['attributes']
+    should_end_session = True
+
+    speech_output = ""
+    reprompt_text = speech_output
 
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
 # --------------- Events ------------------
-
-def on_session_started(session_started_request, session):
-    """ Called when the session starts """
-
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
 
 
 def on_launch(launch_request, session):
@@ -136,6 +159,14 @@ def on_intent(intent_request, session):
     # Dispatch to your skill's intent handlers
     if intent_name == "answerQuestion":
         return answer_question(intent, session)
+    if intent_name == "mapToWelcome":
+        return get_welcome_response()
+    if intent_name == "AMAZON.HelpIntent":
+        return get_help(intent, session)
+    if intent_name == "AMAZON.StopIntent":
+        return exit_skill(intent, session)
+    if intent_name == "AMAZON.CancelIntent":
+        return exit_skill(intent, session)
     else:
         raise ValueError("Invalid intent")
 
@@ -148,9 +179,16 @@ def on_session_ended(session_ended_request, session):
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
           ", sessionId=" + session['sessionId'])
     # add cleanup logic here
+    card_title = "Session Ended"
+    speech_output = "See ya! "
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
 
 
 # --------------- Main handler ------------------
+
 
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
@@ -158,19 +196,6 @@ def lambda_handler(event, context):
     """
     print("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
-
-    """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
-
-    if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
 
     if event['request']['type'] == "LaunchRequest":
         return on_launch(event['request'], event['session'])
